fec4f6bfb0d2083ee3e23993d6d57a8df63ceb4f
nytu03 <szymus.nytus@gmail.com>
2023-03-07 08:45:40 +0100

    zmiana

--- shp_conversion.py
+++ shp_conversion.py
@@ -3,3 +3,4 @@ import geopandas
 shp_file = geopandas.read_file(r"C:\Users\snytko\Desktop\projekty\load\True_Ortho_0953_62969.shp")
 shp_file.to_file("json_output.geojson", driver="GeoJSON")
 
+# zmiana
